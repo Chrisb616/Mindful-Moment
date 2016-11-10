a62e9e4b95e0156cfb656ac55c040aa96a4c45a8
Christopher Boynton <flatironschool@Flatirons-MacBook-Air-67.local>
2016-11-10 15:08:41 -0500

    Lots of good changes

--- Flatiron Presents Health Kit App/ViewController.swift
+++ Flatiron Presents Health Kit App/ViewController.swift
@@ -10,60 +10,108 @@ import UIKit
 import HealthKit
 class ViewController: UIViewController {
     
-    var backgroundColor01 = UIColor.init(red: 143/255, green: 201/255, blue: 185/255, alpha: 1)
-    var backgroundColor02 = UIColor.init(red: 216/255, green: 217/255, blue: 192/255, alpha: 1)
-    var backgroundColor03 = UIColor.init(red: 209/255, green: 142/255, blue: 143/255, alpha: 1)
-    var backgroundColor04 = UIColor.init(red: 171/255, green: 92/255, blue: 114/255, alpha: 1)
-    var backgroundColor05 = UIColor.init(red: 145/255, green: 51/255, blue: 79/255, alpha: 1)
-    var backgroundColor06 = UIColor()
-    var backgroundColor07 = UIColor()
-    var backgroundColor08 = UIColor()
-    var backgroundColor09 = UIColor()
-    var backgroundColor10 = UIColor()
+    let healthStore = HealthStore.healthStore
     
+    @IBOutlet weak var startLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    let startButtonTapGesture = UIGestureRecognizer()
     
-    let healthStore = HKHealthStore()
+    var countIsActive = false
+    
+    var timer = Timer()
+    var secondsInTime = 0
+    
+    var startTime = Date()
+    var endTime = Date()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let date = Date()
-        let mintes: UInt = 15
-        print(date)
-        print(mintes)
+        setUpViews()
         
+        let queue = OperationQueue()
         
-        
-        
-        let typestoRead = Set([
-            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession)!
-            ])
-        
-        let typestoShare = Set([
-            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession)!
-            ])
-        
-        self.healthStore.requestAuthorization(toShare: typestoShare, read: typestoRead) { (success, error) -> Void in
-            if success {
-                print("Health Kit Authorized")
-            } else {
-                print("Health Kit Authorization Denied")
-                if error != nil {
-                    print("\(error.debugDescription)")
-                }
-            }
+        queue.addOperation {
+            HealthStore.setUpHealthStore()
         }
         
-        sleep(60)
-        saveMeditation(seconds: date.timeIntervalSinceNow)
+        
+        print("View Did load")
+        
+        
+        
+        self.view.layoutIfNeeded()
+        
+        
+        //let date = Date()
+        //saveMeditation(seconds: date.timeIntervalSinceNow)
     }
     
     
+    func setUpViews() {
+        print(startButton.allTargets)
+        startButton.layer.cornerRadius = 10
+        startButton.layer.borderColor = UIColor.purpleMountain.cgColor
+        startButton.layer.borderWidth = 3
+        startButton.layer.shadowColor = UIColor.black.cgColor
+        startButton.layer.shadowOpacity = 1
+        startButton.layer.shadowOffset = CGSize(width: 2, height: 5)
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        print(startButton.allTargets)
+        startButton.addSubview(startLabel)
+        //startButton.layer.shadowRadius = 5
+    }
     
+    func startButtonTapped(_ sender: UIButton) {
+        if countIsActive {
+            countIsActive = false
+            
+            print("Stop Button Tapped")
+            sender.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            startLabel.text = "Start"
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                sender.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.view.layoutIfNeeded()
+            })
+            
+            
+            endTimer()
+            saveMeditation(start: startTime, end: endTime)
+            
+        } else {
+            print("Start Button Tapped")
+            sender.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            startLabel.text = "Stop"
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                sender.transform = CGAffineTransform(scaleX: 1, y: 1)
+                self.view.layoutIfNeeded()
+            })
+            
+            countIsActive = true
+            startTimer()
+        }
+    }
     
-    func saveMeditation(seconds: TimeInterval) {
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        startTime = Date()
+    }
+    func endTimer() {
+        timer.invalidate()
+        endTime = Date()
+        secondsInTime = 0
+    }
+    func updateTime(_ sender: Timer) {
+        secondsInTime += 1
+        print(secondsInTime)
+    }
+    
+    func saveMeditation(start: Date, end: Date) {
         let mindfulType = HKCategoryType.categoryType(forIdentifier: .mindfulSession)
-        let mindfulSample = HKCategorySample(type: mindfulType!, value: 0, start: Date.init(timeIntervalSinceNow: seconds ), end: Date())
+        let mindfulSample = HKCategorySample(type: mindfulType!, value: 0, start: start, end: end)
         healthStore.save(mindfulSample) { success, error in
             if(error != nil) {
                 print(error.debugDescription)
