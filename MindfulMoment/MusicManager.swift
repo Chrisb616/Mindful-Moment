1081d236a93465bc2daae462c7e03f09dd1c8e16
Chrisb616 <cboynton16@gmail.com>
2016-12-20 08:24:12 -0500

    File managment: removed images and added music

---
+++ MindfulMoment/MusicManager.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayer.swift
+//  MindfulMoment
+//
+//  Created by Christopher Boynton on 12/20/16.
+//  Copyright © 2016 Christopher. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class MusicManager {
+    
+    
+    
+}
