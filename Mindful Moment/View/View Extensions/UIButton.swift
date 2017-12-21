e8ccd0ade41780d9e367e598fc2ba0789e2a49b5
Christopher Boynton <cboynton16@gmail.com>
2017-12-21 14:53:27 -0500

    Small changes, including adding a standard formatting for UIButtons

---
+++ Mindful Moment/View/View Extensions/UIButton.swift
@@ -0,0 +1,9 @@
+//
+//  UIButton.swift
+//  Mindful Moment
+//
+//  Created by Christopher Boynton on 12/21/17.
+//  Copyright © 2017 Christopher Boynton. All rights reserved.
+//
+
+import Foundation
