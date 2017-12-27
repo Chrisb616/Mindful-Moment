1e7cadf661338ad5db5ea0e95a50025ff1aad9b7
Christopher Boynton <cboynton16@gmail.com>
2017-12-27 12:55:48 -0500

    Basic background set up

--- Mindful Moment/View/View Controllers/BackgroundViewController/BackgroundViewController.swift
+++ Mindful Moment/View/View Controllers/BackgroundViewController/BackgroundViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class BackgroundViewController: UIViewController {
     
+    //MARK: - Stored Properties
     var isFirstLoad = true
     
+    //MARK: View Elements
+    
+    var centerMountainView: UIImageView!
+    var leftMountainView: UIImageView!
+    var rightMountainView: UIImageView!
+    
+    var oceanView: UIView!
+    
+    //MARK: - Instantiation
     static var instanceFromNib: BackgroundViewController {
         return BackgroundViewController(nibName: "BackgroundViewController", bundle: Bundle.main)
     }
 
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpNotifications()
+        setUpInitialBackgroud()
+        setUpNotificationObservers()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -36,7 +49,40 @@ class BackgroundViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func setUpNotifications() {
+    //MARK: - View Set Up
+    
+    func setUpInitialBackgroud() {
+        
+        let screenRect = UIScreen.main.bounds
+        
+        // Init
+        centerMountainView = UIImageView(image: UIImage.mountain)
+        leftMountainView = UIImageView(image: UIImage.mountain)
+        rightMountainView = UIImageView(image: UIImage.mountain)
+        oceanView = UIView()
+        
+        // Set Up
+        centerMountainView.contentMode = .scaleAspectFill
+        leftMountainView.contentMode = .scaleToFill
+        rightMountainView.contentMode = .scaleToFill
+        oceanView.backgroundColor = Colors.oceanBlue
+        
+        // Add to Superview
+        view.addSubview(oceanView)
+        view.addSubview(centerMountainView)
+        view.addSubview(leftMountainView)
+        view.addSubview(rightMountainView)
+        
+        // Format
+        centerMountainView.frame = CGRect(x: screenRect.origin.x, y: screenRect.height * 0.2, width: screenRect.width, height: screenRect.height * 0.8)
+        leftMountainView.frame = CGRect(x: screenRect.width * -0.3, y: screenRect.height * 0.4, width: screenRect.width * 0.9, height: screenRect.height * 0.6)
+        rightMountainView.frame = CGRect(x: screenRect.width * 0.4, y: screenRect.height * 0.4, width: screenRect.width * 0.9, height: screenRect.height * 0.6)
+        oceanView.frame = CGRect(x: screenRect.origin.x, y: screenRect.height * 0.45, width: screenRect.width, height: screenRect.height * 0.55)
+    }
+    
+    //MARK: - Navigation Observers
+    
+    func setUpNotificationObservers() {
         let notificationManager = NotificationManager.instance
         
         notificationManager.setShowMenuViewControllerObserver(observer: self, selector: #selector(presentMenuViewController))
@@ -73,21 +119,5 @@ class BackgroundViewController: UIViewController {
         let appInfo = AppInfoViewController.instanceFromNib
         appInfo.modalPresentationStyle = .overCurrentContext
         present(appInfo, animated: true, completion: nil)
-    }
-    
-    func showAlert(withTitle title: String, message: String, continueAction: (() -> Void)?, cancelAction: (() -> Void)?) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        if let cancelAction = cancelAction {
-            let action = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
-                cancelAction()
-            })
-            alert.addAction(action)
-        }
-        if let continueAction = continueAction {
-            let action = UIAlertAction(title: "Continue", style: .cancel, handler: { (action) in
-                continueAction()
-            })
-            alert.addAction(action)
-        }
     }
 }
---
+++ Mindful Moment/View/View Extensions/UIImage.swift
@@ -0,0 +1,15 @@
+//
+//  UIImage.swift
+//  Mindful Moment
+//
+//  Created by Christopher Boynton on 12/27/17.
+//  Copyright © 2017 Christopher Boynton. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static let mountain: UIImage = UIImage(named: "Mountain")!
+    
+}
